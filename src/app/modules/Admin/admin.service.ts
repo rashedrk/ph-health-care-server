02e5b90c61d61fca5bd8db9011b2c6a9fae47641
rashedrk <rashedrk001@gmail.com>
2024-05-18 21:25:40 +0600

    search optimization

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,26 +1,42 @@
-import { PrismaClient } from "@prisma/client"
+import { Prisma, PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient
 
 const getAllAdminFromDB = async (query: any) => {
+    const andConditions: Prisma.AdminWhereInput[] = [];
+    const adminSearchableFields = ['name', 'email'];
+
+    if (query.searchTerm) {
+        andConditions.push(
+            {
+                // OR: [
+                //     {
+                //         name: {
+                //             contains: query.searchTerm,
+                //             mode: "insensitive"
+                //         }
+                //     },
+                //     {
+                //         email: {
+                //             contains: query.searchTerm,
+                //             mode: "insensitive"
+                //         }
+                //     }
+                // ]     
+                OR: adminSearchableFields.map(filed => ({
+                    [filed]: {
+                        contains: query.searchTerm,
+                        mode: "insensitive"
+                    }
+                }))
+            }
+        )
+    }
+
+    const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
+
     const result = await prisma.admin.findMany({
-        where: {
-            OR: [
-                {
-                    name: {
-                        contains: query.searchTerm,
-                        mode: "insensitive"
-                    }
-                },
-                {
-                    email: {
-                        contains: query.searchTerm,
-                        mode: "insensitive"
-                    }
-                }
-            ]
-            
-        }
+        where: whereConditions
     });
     return result;
 }
